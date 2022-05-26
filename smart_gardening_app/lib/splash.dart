32338b429cd01f87de4b9b24d3742f4e016d9bc7
Luca <luca.rengo@studio.unibo.it>
2022-05-26 17:31:50 +0200

    Add splash and home screens.

---
+++ smart_gardening_app/lib/splash.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  void _navigateToHome() async {
+    await Future.delayed(const Duration(milliseconds: 1800), () {});
+    Navigator.pushReplacement(
+      context, 
+      MaterialPageRoute(
+        builder: (context) => const HomePage(
+          title: "Smart Gardening App"
+        )
+      ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          alignment: Alignment.topRight,
+          padding: const EdgeInsets.only(top: 60.0, right: 30.0),
+          child: const Text(
+            "Smart Gardening App", 
+            style: TextStyle(
+              fontSize: 36,
+              //fontFamily: 'RailWay'
+              )
+            )
+        ),
+      )
+    );
+  }
+}
