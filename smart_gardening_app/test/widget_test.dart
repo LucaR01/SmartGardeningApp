76fcdcf79c13db51299faa374a6ed3af71277a30
Luca <luca.rengo@studio.unibo.it>
2022-05-27 18:38:10 +0200

    Update test

--- smart_gardening_app/test/widget_test.dart
+++ smart_gardening_app/test/widget_test.dart
@@ -11,9 +11,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:smart_gardening_app/main.dart';
 
 void main() {
+  //TODO: remove?
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
