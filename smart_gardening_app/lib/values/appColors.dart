93bc8889528a0389be1d8ba78a6498c1e4c7608c
Luca <luca.rengo@studio.unibo.it>
2022-05-27 18:37:46 +0200

    Add constants.

---
+++ smart_gardening_app/lib/values/appColors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+//TODO: use an enum?
+//TODO: static const?
+//TODO: rename variable name in kPrimaryColor?
+//const Color appMainColor = Color();  //TODO: uncomment when color will be added.
---
+++ smart_gardening_app/lib/values/appStrings.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+//TODO: rename file in constants.dart?
+
+const String appName = "Smart Gardening App"; //TODO: rename in APP_NAME?
