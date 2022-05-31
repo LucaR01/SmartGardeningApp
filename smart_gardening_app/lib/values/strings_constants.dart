fa89560b7240e7248505438557e8016c15c9b71e
Luca <luca.rengo@studio.unibo.it>
2022-05-31 19:11:55 +0200

    Update strings' constants

--- smart_gardening_app/lib/values/strings_constants.dart
+++ smart_gardening_app/lib/values/strings_constants.dart
@@ -1,6 +1,3 @@
 import 'package:flutter/material.dart';
 
-//TODO: rename file in constants.dart?
-//TODO: use k prefix?
-
-const String appName = "Smart Gardening App"; //TODO: rename in APP_NAME?
+const String kAppName = "Smart Gardening App";
