38225c8bab32467fbcbb7acbcc7175983c32667c
Luca <luca.rengo@studio.unibo.it>
2022-05-31 19:00:26 +0200

    Update navigation drawer

--- smart_gardening_app/lib/widgets/drawer/navigation_drawer.dart
+++ smart_gardening_app/lib/widgets/drawer/navigation_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../screens/home/home_screen.dart';
+import '../../screens/pages.dart';
 
 class NavigationDrawerWidget extends StatelessWidget {
   const NavigationDrawerWidget({Key? key}) : super(key: key);
@@ -15,11 +16,14 @@ class NavigationDrawerWidget extends StatelessWidget {
         child: ListView(
           padding: padding, //TODO: remove?
           children: <Widget>[
+            buildHeader(), //TODO: image: img,
+
+            //TODO: volendo aggiungere container con column
             const SizedBox(height: 48),
             buildMenuItem(
               text: 'Home',
               icon: Icons.home,
-              onClicked: () => selectedItem(context, 0),
+              onClicked: () => selectedItem(context, Pages.home),
             ),
             const SizedBox(height: 16),
             buildMenuItem(
@@ -58,17 +62,26 @@ class NavigationDrawerWidget extends StatelessWidget {
             ),
 
             const SizedBox(height: 16),
-            buildMenuItem(
-              text: 'Privacy Policy',
-              icon: Icons.privacy_tip
-              //onClicked: () => selectedItem(context, 6), //TODO: uncomment
-            ),
-
+            buildMenuItem(text: 'Privacy Policy', icon: Icons.privacy_tip
+                //onClicked: () => selectedItem(context, 6), //TODO: uncomment
+                ),
           ],
         ),
       ),
     );
   }
+
+  //TODO: required Image image?
+  Widget buildHeader({VoidCallback? onClicked}) => InkWell(
+        onTap: onClicked,
+        child: Container(
+          child: Row(
+            children: [
+              //CircleAvatar(radius: 30, backgroundImage: Image()), //TODO: add plant image.
+            ],
+          ),
+        ),
+      );
 
   //TODO: ne posso fare una che prende anche colore e un hover color
   //TODO: rinominare text in menuName e icon in menuIcon?
@@ -76,7 +89,7 @@ class NavigationDrawerWidget extends StatelessWidget {
     required String text,
     required IconData icon,
     VoidCallback? onClicked,
-  }){
+  }) {
     final color = Colors.white; //TODO: per i colori usare le costanti.
     final hoverColor = Colors.white70;
 
@@ -84,15 +97,53 @@ class NavigationDrawerWidget extends StatelessWidget {
       leading: Icon(icon, color: color),
       title: Text(text, style: TextStyle(color: color)),
       onTap: onClicked,
-      );
+    );
   }
 
-  void selectedItem(BuildContext context, int index){ //TODO: Usare un enum al posto dell'index intero.
-    switch(index){
-      case 0:
+  void selectedItem(BuildContext context, Pages page) {
+    Navigator.of(context).pop(); // Così si chiude il navigation drawer
+
+    switch (page) {
+      case Pages.home:
         Navigator.of(context).push(MaterialPageRoute(
           builder: (context) => HomePage(title: ""), //TODO: remove title
         ));
+        break;
+      case Pages.myPlants:
+        /*Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => , 
+        ));*/ //TODO: uncomment
+        break;
+      case Pages.tasks:
+        /*Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => , 
+        ));*/ //TODO: uncomment
+        break;
+      case Pages.diagnosis:
+        /*Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => , 
+        ));*/ //TODO: uncomment
+        break;
+      case Pages.settings:
+        /*Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => , 
+        ));*/ //TODO: uncomment
+        break;
+      case Pages.notifications:
+        /*Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => , 
+        ));*/ //TODO: uncomment
+        break;
+      case Pages.privacyPolicy:
+        /*Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => , 
+        ));*/ //TODO: uncomment
+        break;
+      default:
+        /*Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => , 
+        ));*/ //TODO: uncomment
+        break;
     }
   }
 }
