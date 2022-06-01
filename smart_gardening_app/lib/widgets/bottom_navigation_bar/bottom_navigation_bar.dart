6c171061ef93a750581a36f28cbe9f9d66306232
Luca <luca.rengo@studio.unibo.it>
2022-06-01 19:30:01 +0200

    Update bottom navigation bar

--- smart_gardening_app/lib/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
+++ smart_gardening_app/lib/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -9,27 +9,32 @@ class BottomNavigationBarWidget extends StatelessWidget {
       shape: const CircularNotchedRectangle(),
       color: Theme.of(context).colorScheme.primary, //TODO: usare color constants.
       child: IconTheme(
-        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary), //TODO: usare color constants
+        data: IconThemeData(
+            color: Theme.of(context).colorScheme.onPrimary), //TODO: usare color constants
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              IconButton( //TODO: metterlo in una funzione
+              IconButton(
+                //TODO: metterlo in una funzione
                 icon: Icon(Icons.home), //TODO: update icon
                 onPressed: () {},
               ),
-              IconButton( //TODO: metterlo in una funzione
+              IconButton(
+                //TODO: metterlo in una funzione
                 icon: Icon(Icons.home), //TODO: update icon
                 onPressed: () {},
               ),
               const SizedBox(width: 24),
-              IconButton( //TODO: metterlo in una funzione
+              IconButton(
+                //TODO: metterlo in una funzione
                 icon: Icon(Icons.home), //TODO: update icon
                 onPressed: () {},
               ),
-              IconButton( //TODO: metterlo in una funzione
-                icon: Icon(Icons.settings), 
+              IconButton(
+                //TODO: metterlo in una funzione
+                icon: Icon(Icons.settings),
                 onPressed: () {},
               ),
             ],
@@ -38,6 +43,5 @@ class BottomNavigationBarWidget extends StatelessWidget {
       ),
     );
   }
-
   //TODO: fare una funzione per ognuno dei pulsanti.
 }
