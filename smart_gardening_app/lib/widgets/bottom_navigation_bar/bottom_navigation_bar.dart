61bf70c1972331957cbc888380c7875540bd4027
Luca <luca.rengo@studio.unibo.it>
2022-06-01 18:08:19 +0200

    Add bottom navigation bar

---
+++ smart_gardening_app/lib/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationBarWidget extends StatelessWidget {
+  const BottomNavigationBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      color: Theme.of(context).colorScheme.primary, //TODO: usare color constants.
+      child: IconTheme(
+        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary), //TODO: usare color constants
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton( //TODO: metterlo in una funzione
+                icon: Icon(Icons.home), //TODO: update icon
+                onPressed: () {},
+              ),
+              IconButton( //TODO: metterlo in una funzione
+                icon: Icon(Icons.home), //TODO: update icon
+                onPressed: () {},
+              ),
+              const SizedBox(width: 24),
+              IconButton( //TODO: metterlo in una funzione
+                icon: Icon(Icons.home), //TODO: update icon
+                onPressed: () {},
+              ),
+              IconButton( //TODO: metterlo in una funzione
+                icon: Icon(Icons.settings), 
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  //TODO: fare una funzione per ognuno dei pulsanti.
+}
