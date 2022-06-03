92ce1232e7911b4e7f2ee5f4f4887ea4a7171bb4
Luca <luca.rengo@studio.unibo.it>
2022-06-03 19:08:25 +0200

    Add diagnosis' screen

---
+++ smart_gardening_app/lib/screens/diagnosis/diagnosis_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DiagnosisScreen extends StatefulWidget {
+  const DiagnosisScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DiagnosisScreen> createState() => _DiagnosisScreenState();
+}
+
+class _DiagnosisScreenState extends State<DiagnosisScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //TODO: 
+    );
+  }
+}
