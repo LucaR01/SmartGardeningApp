51b6bc879b0fe58c3cbf0e1983fa8c56cc1a3e7c
Luca <luca.rengo@studio.unibo.it>
2022-06-15 19:26:03 +0200

    Update MyPlantsScreen

--- smart_gardening_app/lib/screens/my_plants/my_plants_screen.dart
+++ smart_gardening_app/lib/screens/my_plants/my_plants_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:smart_gardening_app/widgets/FAB/FABWidget.dart';
+import 'package:smart_gardening_app/widgets/bottom_navigation_bar/custom_bottom_navigation_bar.dart';
+import 'package:smart_gardening_app/widgets/plants_list/plants_list.dart';
 
 import '../../widgets/app_bar/app_bar.dart';
 
@@ -13,8 +16,40 @@ class _MyPlantsScreenState extends State<MyPlantsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //TODO:
-      appBar: AppBarWidget(),
+      appBar: const AppBarWidget(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: const FABWidget(),
+      bottomNavigationBar: const CustomBottomNavigationBar(),
+      /*body: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(""),
+          ),
+        ),
+      ),*/
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 30),
+            SizedBox(
+              height: 160,
+              child: Text(
+                'My Plants',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.green[900],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Flexible(
+              child: PlantsList(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
