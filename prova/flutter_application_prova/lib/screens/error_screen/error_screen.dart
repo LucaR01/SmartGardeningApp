16c3116486bc00a146fc37e2655b20fac9809609
Luca <luca.rengo@studio.unibo.it>
2022-08-09 21:18:11 +0200

    Update snackbar error widget

--- prova/flutter_application_prova/lib/screens/error_screen/error_screen.dart
+++ prova/flutter_application_prova/lib/screens/error_screen/error_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+//TODO: rinominare file in flash_card o snackbar_card
+//TODO: fare un enum con SnackBarColor.SUCCESS (verde) e FlashCardColor.ERROR (rosso)
 
 class FlashErrorScreen extends StatelessWidget {
   const FlashErrorScreen({Key? key}) : super(key: key);
+
+  //TODO: final String msg;
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +15,86 @@ class FlashErrorScreen extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Stack( //TODO: Wrap in a widget
+                  clipBehavior: Clip.none,
+                  children: [
+                    Container(
+                      height: 90,
+                      decoration: BoxDecoration(
+                        color: Colors.red, //TODO: usare color constants o magari far inserire come parametro
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: Row(
+                        children: [
+                          const SizedBox(width: 48),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Oh snap!",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white, //TODO: use color constants
+                                  ),
+                                ),
+                                const Spacer(),
+                                Text(
+                                  "Flutter Snackbar",
+                                  style: TextStyle(
+                                    color: Colors.white, //TODO: use color constants
+                                    fontSize: 12,
+                                  ),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20)),
+                        child: SvgPicture.asset(
+                          "assets/icons/bubbles.svg",
+                          width: 40,
+                          height: 48,
+                          color: Colors.deepOrange, //TODO: use color constants rosso oscuro
+                          ),
+                      ),
+                    ),
+                    Positioned(
+                      top: -20,
+                      left: 0,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            "assets/icons/fail.svg",
+                            height: 40,
+                          ),
+                          Positioned(
+                            top: 10,
+                            child: SvgPicture.asset(
+                              "assets/icons/close.svg",
+                              height: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                behavior: SnackBarBehavior.floating,
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+              ),
+            );
           }, 
           child: const Text("Show Message"),
         ),
