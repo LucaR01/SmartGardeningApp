9b4e432b589910254ab4180cf231091cfd6e8b39
Luca <luca.rengo@studio.unibo.it>
2022-09-22 22:27:49 +0200

    Update splash screen to show animation and app's logo

--- prova/flutter_application_prova/lib/screens/splash/splash_screen.dart
+++ prova/flutter_application_prova/lib/screens/splash/splash_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_application_prova/constants/strings_constants.dart';
+import 'package:flutter_application_prova/screens/home/home_screen.dart';
+import 'package:flutter_application_prova/screens/onboarding_screen/onboarding_screen.dart';
 import 'package:flutter_application_prova/screens/pages.dart';
 import 'package:flutter_application_prova/utils/utils.dart';
+
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart'; // Per le PageTransitionType
 
 //TODO: rename class in SplashScreen?
 
@@ -18,7 +24,7 @@ class Splash extends StatefulWidget {
   State<Splash> createState() => _SplashState();
 }
 
-class _SplashState extends State<Splash> {
+class _SplashState extends State<Splash> { //TODO: remove?
   @override
   void initState() {
     super.initState();
@@ -45,8 +51,48 @@ class _SplashState extends State<Splash> {
               color: Theme.of(context).textTheme.bodyText1!.color,
               )
             )
+          /*child: Column(
+            children: <Widget> [
+              Text(
+              kAppName,
+              style: TextStyle(
+                fontSize: 36,
+                //fontFamily: 'RailWay'
+                color: Theme.of(context).textTheme.bodyText1!.color,
+              )),
+              Container(
+                child: Image.asset('assets/'),
+              ),
+            ],
+          ),*/
         ),
       )
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Center(
+        child: Column( //TODO: add Center Widget?
+          children: [
+            Text('Smart Gardening', style: TextStyle(fontSize: 36, color: Theme.of(context).textTheme.bodyText1!.color,)), //TODO: use string constants
+            Image.asset('assets/app_icon/Icon-192.png'), //TODO: Theme.of(context).scaffoldBackgroundColor == Colors.white ? Image.asset('') : Image.asset(''),
+            //Text('Smart Gardening', style: TextStyle(fontSize: 36, color: Theme.of(context).textTheme.bodyText1!.color,)), //TODO: use string constants
+          ],
+        ),
+      ),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor, //TODO: oppure verde, verde-scuro
+      nextScreen: OnBoardingPage(), //TODO: const HomePage(), //TODO: dovrà essere showOnBoarding == false ? HomePage() : OnBoardingPage(),
+      splashIconSize: 256, //TODO: prima era 250
+      duration: 4000,
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.fade,
+      animationDuration: const Duration(seconds: 1),
     );
   }
 }
