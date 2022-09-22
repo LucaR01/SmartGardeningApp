0a194c35d8896dabf22c09aadfd006c82573a4e8
Luca <luca.rengo@studio.unibo.it>
2022-09-22 22:27:02 +0200

    Add privacy policy and terms and conditions screens

---
+++ prova/flutter_application_prova/lib/screens/terms_and_conditions/terms_and_conditions.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_prova/widgets/app_bar/app_bar.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+class TermsAndConditionsPage extends StatelessWidget {
+  const TermsAndConditionsPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const AppBarWidget(),
+      body: FutureBuilder(
+        future: rootBundle.loadString("assets/terms_and_conditions/terms_and_conditions.md"),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if(snapshot.hasData) {
+            return Markdown(data: snapshot.data!, );
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(), //TODO: volendo usare SpinKit
+          );
+        }
+      ),
+    );
+  }
+}
